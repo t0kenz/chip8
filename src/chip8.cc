374f710560bb95d8283290ae3b749c25a4365a40
t0kenz <zenorex1@gmail.com>
2022-06-06 20:14:59 +0200

    first commit

---
+++ src/chip8.cc
@@ -0,0 +1,30 @@
+#include "chip8.h"
+#include <cstring>
+
+uint16_t Chip8::fetch_opcode() {
+    // merge the opcode by shifting the first byte and then ORing the second.
+    // this follows from memory being stored as single bytes, therefore the instruction
+    // is placed on 2 different spaces.
+    uint16_t opcode = memory[program_counter] << 8 | memory[program_counter + 1];
+    return opcode;
+
+
+}
+
+void Chip8::execute_instruction(const uint16_t instruction) {
+   // Potential issues: instruction might be 0, meaning we have reached the end or faulty program. 
+}
+
+void Chip8::run_cycle() {
+    uint16_t opcode = fetch_opcode();
+    execute_instruction(opcode);
+}
+
+
+// According to the documentation, the convention is to put
+// all fonts in the memory region of 0x50-0x9F
+void Chip8::load_font() {
+    std::memset(memory, 0x50, sizeof memory); 
+}
+
+
---
+++ src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "chip8.h"
+
+int main(int argc, char** argv) {
+    Chip8 chip8;
+    chip8.test();
+}
+
